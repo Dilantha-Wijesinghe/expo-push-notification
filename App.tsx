2d13f66913dd793dcdf508487b4f33f1601f73f4
Hasith Methmal Jayasekara <hasith@genesiscoretech.com>
2024-05-04 17:26:02 +0530

    [feat] added push notifications for android

--- App.tsx
+++ App.tsx
@@ -1,11 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { usePushNotifications } from "./src/hooks/usePushNotifications";
+import { useEffect } from "react";
+
+/**
+ * Note from Hasith:
+ *
+ * Hi guys, looks like it's not as straight forward as I hoped to be, but this video helped a lot
+ * https://www.youtube.com/watch?v=V-hois8dgM4
+ *
+ *
+ */
 
 export default function App() {
+  const { expoPushToken, notification } = usePushNotifications();
+  const data = JSON.stringify(notification, undefined, 2);
+
+  useEffect(() => {
+    console.log(expoPushToken?.data);
+  }, [expoPushToken]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Token: {expoPushToken?.data ?? ""}</Text>
+      <Text>Notification: {data}</Text>
     </View>
   );
 }
@@ -13,8 +31,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
